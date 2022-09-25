16dd9566024106d398bea370fd98c43fb6b98664
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-09-24 17:26:15 -0700

    third commit

--- algosRecursion.py
+++ algosRecursion.py
@@ -1,3 +1,6 @@
+from math import factorial
+
+
 def iterative_factorial(n):
     fact = 1
     for i in range(2,n+1):
@@ -28,3 +31,21 @@ def permute(string, pocket=""):
             permute(together,letter + pocket)
 
 print(permute("ABCD",""))
+
+def permutations(str):
+    for p in range(factorial(len(str))):
+        print(''.join(str))
+        i = len(str) - 1
+        while i > 0 and str[i - 1] > str[i]:
+            i -= 1
+        str[i:] = reversed(str[i:])
+        if i > 0:
+            q = i
+            while str[i - 1] > str[q]:
+                q += 1
+            temp = str[i - 1]
+            str[i - 1] = str[q]
+            str[q] = temp
+s = 'abc'
+s = list(s)
+print(permutations(s))
